5eeaf3854497774e99a9b8db36681673b13842ae
pengwenchen <domokun0413@gmail.com>
2020-03-19 20:21:50 +0800

    ASR change model name

--- hw1/reproduce.sh
+++ hw1/reproduce.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/s/ksv4414z343gf6b/best_att.pth?dl=0
-wget https://www.dropbox.com/s/5i9g36iw8id23nk/best_ppx.pth?dl=0
+wget https://www.dropbox.com/s/ksv4414z343gf6b/best_att.pth
+wget https://www.dropbox.com/s/5i9g36iw8id23nk/best_ppx.pth
 python3 reproduce.py --config ./config/decode_dlhlp.yaml --test --njobs 6 --testdir $1
 python3 format.py ./result/decode_dlhlp_test_output.csv $2
