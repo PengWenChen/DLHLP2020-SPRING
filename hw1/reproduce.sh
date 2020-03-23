b48fcea3d1641ba8892125e7059a46fe05ba0b9a
pengwenchen <domokun0413@gmail.com>
2020-03-23 22:22:14 +0800

    modify data path

--- hw1/reproduce.sh
+++ hw1/reproduce.sh
@@ -1,4 +1,6 @@
 wget https://www.dropbox.com/s/ksv4414z343gf6b/best_att.pth
 wget https://www.dropbox.com/s/5i9g36iw8id23nk/best_ppx.pth
-python3 reproduce.py --config ./config/decode_dlhlp.yaml --test --njobs 6 --testdir $1
+part1=`dirname "$1"`
+part2=`basename "$1"`
+python3 reproduce.py --config ./config/decode_dlhlp.yaml --test --njobs 4 --testdir $part1
 python3 format.py ./result/decode_dlhlp_test_output.csv $2
