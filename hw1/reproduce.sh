f34fce289b2655b9c86ffa7ea64bd45a4283838a
pengwenchen <domokun0413@gmail.com>
2020-03-23 23:33:01 +0800

    njob change from 4 to 8

--- hw1/reproduce.sh
+++ hw1/reproduce.sh
@@ -2,5 +2,5 @@ wget https://www.dropbox.com/s/ksv4414z343gf6b/best_att.pth
 wget https://www.dropbox.com/s/5i9g36iw8id23nk/best_ppx.pth
 part1=`dirname "$1"`
 part2=`basename "$1"`
-python3 reproduce.py --config ./config/decode_dlhlp.yaml --test --njobs 4 --testdir $part1
+python3 reproduce.py --config ./config/decode_dlhlp.yaml --test --njobs 8 --testdir $part1
 python3 format.py ./result/decode_dlhlp_test_output.csv $2
